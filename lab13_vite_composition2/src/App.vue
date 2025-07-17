07fc9dafe0b991498b41fe4f7012123c150c0dce
Andy Chen <as79726@gmail.com>
2025-07-17 16:59:57 +0800

    finish day4

--- lab13_vite_composition2/src/App.vue
+++ lab13_vite_composition2/src/App.vue
@@ -1,47 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Composition Lab2</h1>
+  <br />
+  <MyComp1></MyComp1>
+    <br />
+  <button @click="toggleOptions2">show/hide options component</button>
+  <br/>
+  <div v-if="displayOptions2">
+    <MyOptions2></MyOptions2>
+  </div>
+  <br/>
+  <br />
+  <button @click="toggleComposition2">show/hide composition component</button>
+  <div v-if="displayComposition2">
+    <MyComposition2></MyComposition2>
+  </div>
+  <br/>
+  <FirebasePost></FirebasePost>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import MyComp1 from './components/MyComp1.vue';
+import MyComposition2 from './components/MyComposition2.vue';
+import MyOptions2 from './components/MyOptions2.vue';
+import FirebasePost from './components/FirebasePost.vue';
+import { ref } from 'vue';
+const displayOptions2 = ref(true);
+const displayComposition2 = ref(true);
+const toggleOptions2 = () => {
+  displayOptions2.value = !displayOptions2.value
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+const toggleComposition2 = () => {
+  displayComposition2.value = !displayComposition2.value
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
+<style scoped></style>
---
+++ lab13_vite_composition2/src/components/MyComposition2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>composition API的生命周期</h1>
+    </div>
+</template>
+
+<script>
+    import { onMounted,onBeforeUnmount } from 'vue';
+    export default {
+        setup(){
+            console.log("元件初始化");
+            onMounted(function(){
+                console.log("composition api掛載之後");
+            });
+
+            onBeforeUnmount(function(){
+                console.log("composition即將移除元件");
+            });
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
